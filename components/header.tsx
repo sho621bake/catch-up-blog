71a84af7e2c76859cf6d97a1be59d225e0f83114
sho621bake <sho6217@gmail.com>
2022-08-12 16:43:38 +0900

    Headerコンポーネント作成

---
+++ components/header.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Header = () => {
+    return (
+        <section className='mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0'>
+            <header className='flex items-center justify-between py-10'>
+                <div className='flex items-center justify-between'>
+                    <Image
+                        src='/image/logo.png'
+                        alt='logo icon'
+                        loading='eager'
+                        width={35}
+                        height={35}
+                    />
+                </div>
+                <h1 className='tracking-normal text-left'>
+                    <Link href='/'>
+                        <a>
+                            <span className='font-normal'>Catch</span>
+                            <span className='font-semibold'>Up's</span>
+                        </a>
+                    </Link>
+                </h1>
+            </header>
+        </section>
+    )
+}
+export default Header
