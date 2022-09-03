f55f977876546957489c0598cf05b2239eb84be0
sho621bake <sho6217@gmail.com>
2022-09-03 09:09:40 +0900

    Update Article.tsx

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,4 +1,5 @@
 import type { Articles } from 'types/article'
+import Image from 'next/image'
 
 type Props = {
     articles: Articles[]
@@ -10,17 +11,22 @@ const Article = ({ articles }: Props) => {
             {articles.map((article) => (
                 <a href={`/articles/${article.id}`} key={article.id}>
                     <div className='card card-side mt-10 bg-base-200 shadow-xl'>
-                        <div className='card-body'>
-                            <h2 className='card-title'>{article.title}</h2>
-                        </div>
-                        <div>
-                            <img
-                                src={article.eyecatch.url}
-                                alt='content-eye-catch'
-                                width={300}
-                                height={300}
-                                loading='eager'
-                            />
+                        <div className='flex w-full justify-between'>
+                            <div className='card-body'>
+                                <h2 className='card-title'>{article.title}</h2>
+                            </div>
+                            <div className='block'>
+                                <div style={{ width: 300, height: 200 }}>
+                                    <Image
+                                        src={article.eyecatch.url}
+                                        alt='content-eye-catch'
+                                        width={300}
+                                        height={200}
+                                        loading='eager'
+                                        layout='responsive'
+                                    />
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </a>
