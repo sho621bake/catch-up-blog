447b9f44c01851be090c6999244ffb18a4727050
sho621bake <sho6217@gmail.com>
2022-09-25 15:50:08 +0900

    テキスト変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,14 +10,13 @@ const Home = (props: any) => {
                 <div className='flex h-screen flex-col justify-between'>
                     <Header />
                     <p className='text-center text-sm'>
-                        CatchUp'sはプログラミング言語やフレームワークなどのリリース情報や
-                        技術情報を配信するブログサイトです。
+                        CatchUp'sはプログラミング言語やフレームワークなどのリリース情報や技術情報を配信するブログサイトです。
                     </p>
                     <main className='mb-auto'>
                         <div className='space-y-4 pt-6 pb-8 md:space-y-6'>
                             <div className='mb-10 border-b-2 border-gray-300'>
                                 <h1 className='md:leading-14 pb-4 text-4xl font-extrabold leading-9 tracking-tight sm:text-4xl sm:leading-10 md:text-4xl'>
-                                    News
+                                    記事一覧
                                 </h1>
                             </div>
                             <Article articles={props.articles} />
